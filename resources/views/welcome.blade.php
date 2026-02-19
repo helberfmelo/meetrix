2c975d17a4922db3024f534e9f744af603562720
helberfmelo <helberfmelo@bigbangdigital.com.br>
2026-02-19 16:09:33 -0300

    chore: cleanup temporary production routes and maintenance triggers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,3 @@
-@if(request('deploy_cmd') == 'meetrix-deploy-secret-2026')
-    @php
-        $cmd = request('cmd', 'optimize:clear');
-        \Illuminate\Support\Facades\Artisan::call($cmd, ['--force' => true]);
-        echo "<h1>Command: $cmd</h1><pre>" . \Illuminate\Support\Facades\Artisan::output() . "</pre>";
-        exit;
-    @endphp
-@endif
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>

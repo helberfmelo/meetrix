5875126b616b411e3c421dff8b2083c50c60aac1
helberfmelo <helberfmelo@bigbangdigital.com.br>
2026-02-19 19:14:36 -0300

    feat: migration to ensure master admin user existence on production

---
+++ database/migrations/2026_02_19_221420_ensure_master_admin_user.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        \App\Models\User::updateOrCreate(
+            ['email' => 'admin@meetrix.pro'],
+            [
+                'name' => 'Master Admin',
+                'password' => \Illuminate\Support\Facades\Hash::make('password'),
+                'is_master_admin' => true,
+                'email_verified_at' => now(),
+            ]
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};

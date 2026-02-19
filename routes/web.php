1304faf23bd76b952d72ece2e44deb9481b81b19
helberfmelo <helberfmelo@bigbangdigital.com.br>
2026-02-19 19:59:30 -0300

    fix: relocate diagnostic routes to web.php for cleaner verification

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
-<?php
-
 use Illuminate\Support\Facades\Route;
+
+        'time' => now()->toDateTimeString()
+    ];
+});
 
 Route::middleware(['auth:sanctum', 'superadmin'])->prefix('super-admin')->group(function () {
     Route::get('/', [\App\Http\Controllers\SuperAdmin\DashboardController::class, 'index'])->name('superadmin.dashboard');
